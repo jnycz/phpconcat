af60bb9fde261bccb45749a854902bd135f17b8b
john.nycz <john.nycz@brunellocreative.com>
2014-11-20 11:10:13 -0500

    Checkpoint Commit

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -18,3 +18,46 @@ $.ajax({
 
 */
 
+/**
+ * ConcatPDF runs ajax request to php script for
+ *
+ * @param items
+ */
+
+
+function concatPDF( items ) {
+
+
+
+
+}
+
+
+$(function() {
+
+  $('#controls button').click( function() {
+
+    // Vars
+    var selectedItem = '#grid .grid__item.added';
+    var selectedPDFs = [];
+
+    // Create list of selected pdfs
+    $(selectedItem).each( function() {
+      selectedPDFs.push($(this).find('img').attr('data-pdf'));
+    });
+
+    // Debug
+    console.log(selectedPDFs);
+
+    if(selectedPDFs.length > 0) {
+      // Run concat function - download default
+      concatPDF(selectedPDFs);
+    } else {
+      alert('You didn\'t select any pdfs to concatenate.');
+    }
+
+
+
+  });
+
+});
