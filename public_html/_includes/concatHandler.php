275f36b1dca4797aabec379831716050861dabea
john.nycz <john.nycz@brunellocreative.com>
2014-11-25 16:57:29 -0500

    Updated fpdf class files.

--- public_html/_includes/concatHandler.php
+++ public_html/_includes/concatHandler.php
@@ -10,9 +10,9 @@
 require_once '../PDFMerger/PDFMerger.php';
 
 // Test for post var of pdfs to concatenate
-if(isset($_POST['pdfs'])) {
+//if(isset($_POST['pdfs'])) {
 
-  $items = json_encode($_POST['pdfs']);
+  //$items = json_encode($_POST['pdfs']);
   $timestamp = time();
   $mergedPDF = $timestamp.'.pdf';
   $pdfDir = '/Applications/XAMPP/xamppfiles/htdocs/sites/phpconcat/public_html/files/pdf/';
@@ -23,8 +23,7 @@ if(isset($_POST['pdfs'])) {
 
   $pdf->addPDF($pdfDir.'bally.pdf', 'all')
     ->addPDF($pdfDir.'bike.pdf', 'all')
-    ->addPDF($pdfDir.'dam.pdf', 'all')
-    ->merge('download', $pdfDir.$timestamp.'.pdf');
+    ->merge('download', $timestamp.'.pdf');
 
   // We'll be outputting a PDF
   header('Content-type: application/pdf');
@@ -35,7 +34,16 @@ if(isset($_POST['pdfs'])) {
   // The PDF source is in original.pdf
   readfile('original.pdf');
 
-}
+//}
 
 //REPLACE 'file' WITH 'browser', 'download', 'string', or 'file' for output options
 //You do not need to give a file path for browser, string, or download - just the name.
+
+
+
+
+/*
+ * NOTES
+ * THis is working when I hit the php scripti directly.  WE need to return the url of the merged pdf
+ * and display it in an iframe for the download to work.
+ */
